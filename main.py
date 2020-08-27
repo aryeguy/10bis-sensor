c85d18107cc2daa7a1e00e1a77df5fd9414b2ccb
aryeguy <arye.guy@gmail.com>
2020-08-27 12:24:49 +0300

    Running headless

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import os
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+
 import time
 
 USERNAME = os.environ['USERNAME']
 PASSWORD = os.environ['PASSWORD']
+options = Options()
+options.headless = True
 
-driver = webdriver.Firefox()
+driver = webdriver.Firefox(options=options)
 driver.get("https://www.10bis.co.il/")
 driver.find_element_by_css_selector("button.styled__HeaderUserLink-sc-1l9k7ll-4:nth-child(3)").click()
 driver.find_element_by_css_selector("#email").send_keys(USERNAME)
@@ -21,5 +25,4 @@ time.sleep(10)
 credit = driver.find_element_by_css_selector(".klnjjw").text
 # TODO clean currency from text
 print(credit)
-input()
 driver.close()
